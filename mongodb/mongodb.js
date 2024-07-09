3b866d98ec6ceb5036065b2a931384ba908120b6
Alamsher77 <107561278+Alamsher77@users.noreply.github.com>
2024-07-09 12:44:59 +0530

    Update mongodb.js

--- mongodb/mongodb.js
+++ mongodb/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const mongoDb = async ()=> {
   try {
-    await mongoose.connect("mongodb://shopping:shopping1234@ac-xrfmegw-shard-00-00.nvds4xq.mongodb.net:27017,ac-xrfmegw-shard-00-01.nvds4xq.mongodb.net:27017,ac-xrfmegw-shard-00-02.nvds4xq.mongodb.net:27017/?replicaSet=atlas-98ij82-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=ShoppingApp")
+    await mongoose.connect("mongodb+srv://shopping:shopping1234@shoppingapp.nvds4xq.mongodb.net/?retryWrites=true&w=majority&appName=ShoppingApp")
     console.log('connected to db')
   }catch(error) {
     console.log(error)
