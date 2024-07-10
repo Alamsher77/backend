8168094b28f520aa9e249f0cfa0cd9cb05e7f1f1
Alamsher77 <107561278+Alamsher77@users.noreply.github.com>
2024-07-10 21:46:59 +0530

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -3,20 +3,15 @@ const authenticateJWT = (req, res, next) => {
   
  const token = req.cookies?.token 
 
- console.log(token)
- res.json({
-  message:token
- })
- /*  if (!token) {
+
+  if (!token) {
    res.status(400).json({
      success:false,
      message:'Please login'+token,
      data:token
     })
-      return false
+      return false 
     }
-   */
- return false
 
     jwt.verify(token,"ahsdjehorwejdhfowrwerijlksfjasf", (err, email) => {
      if(err){
