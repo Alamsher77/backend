1f6a5b156a5f213b65b4d6bbb04bbabacccfe726
Alamsher77 <107561278+Alamsher77@users.noreply.github.com>
2024-07-11 16:31:36 +0530

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -1,7 +1,7 @@
  import jwt from 'jsonwebtoken'
 const authenticateJWT = (req, res, next) => {
   
- const token = req.cookies?.token
+ const token = req?.cookies
   if (!token) {
    res.status(400).json({
      success:false,
