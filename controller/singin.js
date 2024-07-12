04ac27cebdb41d3777c5ee8d14d0914dc5def013
Alamsher77 <107561278+Alamsher77@users.noreply.github.com>
2024-07-12 07:36:25 +0530

    Update singin.js

--- controller/singin.js
+++ controller/singin.js
@@ -43,8 +43,9 @@ const userSingInController = async (req,res)=>{
  
   
   res.cookie('token', token, {
-    maxAge: 2592000000,
-    httpOnly: true
+    secure: true,
+    httpOnly: true,
+    sameSite:"None"
   });
  
 
