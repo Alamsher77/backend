a7e6dd40c2e9c5e4f124bcd7de0fe53f722f14d7
Alamsher77 <alamsheransari15@gmail.com>
2024-08-22 10:06:01 +0530

    domai

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
  app.use(express.json()) 
 
  app.use(cors({
-   origin:"http://localhost:5173",
+   origin:"https://master--onlineshopes.netlify.app",
    credentials:true
  }))
  
