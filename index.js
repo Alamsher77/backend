613257df6fd5963dc7cf21b06ea750dd23abd3b7
Alamsher77 <alamsheransari15@gmail.com>
2024-11-03 18:00:32 +0530

    file changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import multer from 'multer'
 import path  from 'path'
 import mongoDb from './mongodb/mongodb.js'
 import router from './route/index.js'
@@ -10,18 +9,18 @@ dotenv.config()
 const app = express()
  app.use(express.json()) 
 //const url = 'http://localhost:5173'
- const url = "https://easyshopemart.netlify.app"
+const url = "https://easyshopemart.netlify.app"
 app.use(cors({
   origin:url,
   credentials:true
 })) 
+
 app.use(cookieParser())
  
  
  app.use('/api',router)
- app.use('/image',express.static('uploads/image'))
  
-const PORT = process.env.PORT || 4000 
+const PORT = 4000 
 
 app.get('/',(req,res)=>{
   res.send('app running on global')
@@ -33,4 +32,3 @@ mongoDb().then(()=>{
   console.log(`server is running port ${PORT}`)
 })
 })
-
--- route/index.js
+++ route/index.js
@@ -5,7 +5,6 @@ import path from 'path'
  import userSingInController from '../controller/singin.js'
  import showuser from '../controller/showuser.js'
  import imageDelete from '../controller/delete.image.js'
-import {upload,response} from '../controller/upload.image.js'
 import  {addProduct,showProduct} from '../controller/product.add.js'
  import {productCategry,showProductCategry,removeCategry} from '../controller/product.categry.js'
  import  authenticateJWT from '../auth/auth.js'
@@ -28,8 +27,6 @@ import {addbanner,showallbanners,deletebanner} from '../controller/addbanner.js'
 import {productreview,showProductreview,deleteReview} from '../controller/productreview.js'
 const router = express.Router()
  
-// upload image api
- router.post('/uploadImage',upload.single('product'),response)
  router.delete('/imageDelete',imageDelete)
 // // user singin and singup api
  router.post('/singup',userSinigupController)
