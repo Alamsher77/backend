df57467ffc585bf1a84188371f5b8ee2f70a3cac
Alamsher77 <alamsheransari15@gmail.com>
2025-03-31 15:47:25 +0530

    new version update

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ import cookieParser from 'cookie-parser'
 dotenv.config()   
 const app = express()
  app.use(express.json()) 
-// const url = 'http://localhost:5173'
-const url = "https://easyshopemart.netlify.app"
+const url = 'http://localhost:5173'
+// const url = "https://easyshopemart.netlify.app"
 app.use(cors({
   origin:url,
   credentials:true
